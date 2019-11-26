128592c52ab00864fab90da541aea70326b492a8
Debauche Stéphane <std@odoo.com>
2019-11-26 11:03:26 +0100

    [IMP] mass_mailing_event: add a constrains on the domain
    If the user want to contact attendees, he need to specify the event in the domain.
    
    Task #2119333

--- addons/mass_mailing_event/models/mailing_mailing.py
+++ addons/mass_mailing_event/models/mailing_mailing.py
@@ -1,4 +1,5 @@
-from odoo import models, api
+from odoo import models, api, _
+from odoo.exceptions import UserError
 
 
 class MassMailingCampaign(models.Model):
@@ -11,3 +12,9 @@ class MassMailingCampaign(models.Model):
         if self.mailing_model_name == 'event.registration' and self.mailing_domain == '[]':
             self.mailing_domain = self.env.context.get('default_mailing_domain', '[]')
         return result
+
+    @api.constrains('mailing_model_id', 'mailing_domain')
+    def _check_domain_for_event_registration(self):
+        for mass_mailing in self:
+            if mass_mailing.mailing_model_name == 'event.registration' and 'event_id' not in mass_mailing.mailing_domain:
+                raise UserError(_('You must filter on the event to contact attendee'))
