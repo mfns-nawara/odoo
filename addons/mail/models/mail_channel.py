yf956613/odoo-1

[IMP] mail: prevent email notif when someone join a channel
Purpose of this commit is to dont send email notification when someone
joint a channel, keep only a log note

task-2003281
closes #35142

addons/mail/models/mail_channel.py
@@ -782,7 +782,7 @@ class Channel(models.Model):
                     }
                 else:
                     notification = _('<div class="o_mail_notification">joined <a href="#" class="o_channel_redirect" data-oe-id="%s">#%s</a></div>') % (channel.id, channel.name,)
-                self.message_post(body=notification, message_type="notification", subtype="mail.mt_comment", author_id=partner.id)
+                self.message_post(body=notification, message_type="notification", subtype="mail.mt_comment", author_id=partner.id, notify_by_email=False)
 
         # broadcast the channel header to the added partner
         self._broadcast(partner_ids)

mfns-nawara/odoo

addons/mail/models/mail_channel.py
@@ -780,7 +780,7 @@ class Channel(models.Model):
                     }
                 else:
                     notification = _('<div class="o_mail_notification">joined <a href="#" class="o_channel_redirect" data-oe-id="%s">#%s</a></div>') % (channel.id, channel.name,)
-                self.message_post(body=notification, message_type="notification", subtype_xmlid="mail.mt_comment", author_id=partner.id)
+                self.message_post(body=notification, message_type="notification", subtype_xmlid="mail.mt_comment", author_id=partner.id, notify_by_email=False)
 
         # broadcast the channel header to the added partner
         self._broadcast(partner_ids)