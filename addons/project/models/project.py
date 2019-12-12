8609ee47136421c8f030c69fc2e3ecdf765a3653
Barad Mahendra <mba@openerp.com>
2019-12-12 14:24:30 +0530

    [FIX] project: wrong message parent_id passed on creation of subtask
    Current behaviour:
      when project's first stage have email template set and going to create
    a sub task at that time it will going to set the parent_id on message because
    on action_subtask method the default_parent_id is passed and it is continue to
    passing though the mail message from _track_template and due to wrong
    parent_id(parent task's ID not for parent message) it was giving the error
    of mail.message record not found.
    
    so when message is going to send from _track_template set the parent_id False
    because on default_get parent_id is of parent Task not parent message.
    
    Task-2129052
    Closes: #41092

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -759,6 +759,7 @@ class Task(models.Model):
         if 'stage_id' in changes and test_task.stage_id.mail_template_id:
             res['stage_id'] = (test_task.stage_id.mail_template_id, {
                 'auto_delete_message': True,
+                'parent_id': False,
                 'subtype_id': self.env['ir.model.data'].xmlid_to_res_id('mail.mt_note'),
                 'email_layout_xmlid': 'mail.mail_notification_light'
             })
