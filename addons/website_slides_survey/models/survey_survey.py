5ddf67498574aa21676017a5f29e45c69897b394
Tiffany Chang (tic) <tic@odoo.com>
2019-12-23 15:16:26 +0100

    [IMP]website_slides_survey: add survey course visibility
    Previously there was no way to view when a survey (specifically a
    certification survey) was being used by a course from the survey
    app. We handle 2 cases for when a survey is currently assigned to
    a course:
    
    1. A survey is assigned to 1 course => survey form stat button visible to bring user
       directly to corresponding course form.
    2. A survey is assigned to more than 1 course (unlikely, but currently possible) =>
       survey form stat button visible to show user a list of corresponding courses where
       each list entry can be clicked on to view corresponding course form.
    
    Note: we do not cover viewing courses a survey was previously assigned to as this would
       be currently be unreasonable to track.
    
    Task: 2153743

--- addons/website_slides_survey/models/survey_survey.py
+++ addons/website_slides_survey/models/survey_survey.py
@@ -1,11 +1,47 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import models, fields, api
 
 
 class Survey(models.Model):
     _inherit = 'survey.survey'
+    slide_ids = fields.One2many('slide.slide', 'survey_id', 'Certification')
+    slide_channel_ids = fields.One2many('slide.channel', compute='_compute_slide_channel_ids')
+    courses_count = fields.Integer("Linked Courses", compute="_compute_slide_channel_ids")
+
+    @api.depends('slide_ids')
+    def _compute_slide_channel_ids(self):
+        for survey in self:
+            if survey.slide_ids:
+                survey.slide_channel_ids = survey.slide_ids.mapped('channel_id')
+            else:
+                survey.slide_channel_ids = self.env['slide.channel']
+            survey.courses_count = len(survey.slide_channel_ids)
+
+    # ---------------------------------------------------------
+    # Actions
+    # ---------------------------------------------------------
+
+    def action_survey_view_courses(self):
+        action_vals = {
+            'name': 'Certification Courses',
+            'type': 'ir.actions.act_window',
+            'res_model': 'slide.channel',
+            'view_ids': [self.env.ref('website_slides.slide_channel_view_tree').id,
+                         self.env.ref('website_slides.view_slide_channel_form').id],
+        }
+        if self.courses_count == 1:
+            action_vals.update({'view_mode': 'form',
+                                'res_id': self.slide_channel_ids[0].id})
+        else:
+            action_vals.update({'view_mode':'tree,form',
+                                'domain': [('id', 'in', self.slide_channel_ids.ids)]})
+        return action_vals
+
+    # ---------------------------------------------------------
+    # Business
+    # ---------------------------------------------------------
 
     def _check_answer_creation(self, user, partner, email, test_entry=False, check_attempts=True, invite_token=False):
         """ Overridden to allow website_slides_officer to test certifications. """
